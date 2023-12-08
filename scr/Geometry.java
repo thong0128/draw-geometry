cd01c9641b33ea6d75353e55665ee2787ffaa2ed
thong0128 <thong0128@gmail.com>
2023-12-08 16:11:06 +0700

    Update

--- scr/Geometry.java
+++ scr/Geometry.java
@@ -3,39 +3,50 @@ import java.util.Scanner;
 public class Geometry {
     public static void main(String[] args) {
         int choice = -1;
+        int width = 5, height = 6;
         Scanner input = new Scanner(System.in);
         do {
             System.out.println("Menu");
-            System.out.println("1. Draw the triangle");
-            System.out.println("2. Draw the square");
-            System.out.println("3. Draw the rectangle");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
             choice = input.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Draw the triangle");
-                    System.out.println("******");
-                    System.out.println("*****");
-                    System.out.println("****");
-                    System.out.println("***");
-                    System.out.println("**");
-                    System.out.println("*");
+                    for (int i = 0; i < height ; i++) {
+                        for (int j = 0; j < width; j++) {
+                            System.out.print(" * ");
+                        }
+                        System.out.println();
+                    }
                     break;
                 case 2:
-                    System.out.println("Draw the square");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
+                    for (int i = 0; i <= height ; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print(" * ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                    for (int i = height; i >= 1 ; i--) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print(" * ");
+                        }
+                        System.out.println();
+                    }
                     break;
                 case 3:
-                    System.out.println("Draw the rectangle");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
+                    for (int i = 0; i < height ; i++) {
+                        for (int j = height - i -1; j >= 0 ; j--) {
+                            System.out.print("   ");
+                        }
+                        for (int j = 0; j < 2*i + 1; j++) {
+                            System.out.print(" * ");
+                        }
+                        System.out.println();
+                    }
                     break;
                 case 0:
                     System.exit(0);
